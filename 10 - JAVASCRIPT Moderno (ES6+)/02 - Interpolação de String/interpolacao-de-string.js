8e6dc3733ea148bda99574b3ad099e0aa58e56d7
leandroalvesmachado <leandroalvesmachado@gmail.com>
2022-03-10 14:37:15 -0300

    iniciando novo modulo js es6

---
+++ 10 - JAVASCRIPT Moderno (ES6+)/02 - Interpolação de String/interpolacao-de-string.js
@@ -0,0 +1,18 @@
+let spaceshipName = "Colossus"
+
+// Nome da nave: Colossus
+console.log("Nome da nave: " + spaceshipName)
+// Nome da nave: Colossus
+console.log(`Nome da nave: ${spaceshipName}`)
+
+
+let newName = `Nome da nave: ${spaceshipName}`
+
+// Nome da nave: Colossus
+console.log(newName)
+
+
+let velocity = 50
+
+// Velocidade da nave: 50
+console.log(`Velocidade da nave: ${velocity} km/s`)
